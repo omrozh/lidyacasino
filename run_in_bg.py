6a6b8bcb492870040c38c5c01e817bd3d7098c94
omrozh <53612475+omrozh@users.noreply.github.com>
2024-05-17 10:32:53 +0300

    sports ui

--- run_in_bg.py
+++ run_in_bg.py
@@ -1,8 +1,16 @@
 from app import run_pending_jobs, app
+from betting_utils import live_betting, register_open_bet
+import time
+
+total_minutes = 0
 
 with app.app_context():
     try:
-        run_pending_jobs()
+        if total_minutes == 120:
+            register_open_bet()
+            total_minutes = 0
+        live_betting()
+        time.sleep(60)
     except:
         pass
 
