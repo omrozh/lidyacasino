d90dee5b6585e8e23cccd913ec2caeaf77320e5a
omrozh <53612475+omrozh@users.noreply.github.com>
2024-05-24 13:29:29 +0300

    admin updates

--- app.py
+++ app.py
@@ -2871,9 +2871,7 @@ def casino_result_bet():
     from casino_utils import get_games, get_providers
 
     for c in get_games().get("games"):
-        print(c)
-        print(flask.request.values.get("gameId"))
-        if int(flask.request.values.get('gameId')) in int(c.get("gameId")):
+        if int(flask.request.values.get('gameId')) in int(c.get("id")):
             game_info = f"{c.get('type')} | {c.get('name')}"
             provider_id = c.get("providerId")
 
