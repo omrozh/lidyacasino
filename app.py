bc0b95de13360a80c34ab8daac3ba65a6e3da07c
omrozh <53612475+omrozh@users.noreply.github.com>
2024-05-29 18:15:29 +0300

    GrandCasino

--- app.py
+++ app.py
@@ -1891,8 +1891,6 @@ def admin_portal():
 
 @app.route("/bahis")
 def bahis():
-    import time
-    start_time = time.time()
     offset = int(flask.request.args.get("offset", 0))
     sport = flask.request.args.get("sport", None)
     league = flask.request.args.get("league", None)
@@ -1923,8 +1921,6 @@ def bahis():
     open_bets = open_bets.all()
 
     number_of_chunks = range(int(len(open_bets) / 50) + 1)
-
-    print(time.time() - start_time)
 
     return flask.render_template("bahis/bahis-yeni.html", open_bets=open_bets[offset * 50:(offset + 1) * 50],
                                  canli_bahis=False,
