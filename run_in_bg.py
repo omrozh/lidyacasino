179faa6ec46907413a9e7406208c0828ca0534f3
omrozh <53612475+omrozh@users.noreply.github.com>
2024-06-08 18:04:57 +0300

    lidya casino

--- run_in_bg.py
+++ run_in_bg.py
@@ -2,17 +2,17 @@ from app import app
 from betting_utils import live_betting, register_open_bet
 import time
 
-total_minutes = 0
+total_seconds = 0
 
 with app.app_context():
     while True:
         print(total_minutes)
-        if total_minutes == 120:
+        if total_seconds > 7200:
             register_open_bet()
             total_minutes = 0
         live_betting()
-        total_minutes += 1
-        time.sleep(60)
+        total_seconds += 3
+        time.sleep(3)
 
 # latest process id: kmcron
 # tmux attach-session -t kmcron
