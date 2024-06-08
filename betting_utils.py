8b86f7705937285bfe5a9d856513c12e96bde385
omrozh <53612475+omrozh@users.noreply.github.com>
2024-06-08 20:12:27 +0300

    h

--- betting_utils.py
+++ betting_utils.py
@@ -12,6 +12,7 @@ api_key = "na"
 def get_live_score(open_bet):
     r = requests.get("https://www.thesportsdb.com/api/v1/json/3/latestsoccer.php")
     match_likelihood = 0
+    print(r.json())
     for i in r.json().get("teams"):
         print(i)
         for match in i.get("Match"):
