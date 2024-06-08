54669de43ba537df92b135964c31b74958deb80f
omrozh <53612475+omrozh@users.noreply.github.com>
2024-06-08 19:35:46 +0300

    lidya

--- app.py
+++ app.py
@@ -2078,7 +2078,7 @@ def canli_bahis_mobile():
 def bahis_mac(bahis_id):
     open_bet = OpenBet.query.get(bahis_id)
     from_frame = flask.request.args.get("iframe", False) == "True"
-    is_canli_bahis = open_bet.bet_ending_datetime >= datetime.datetime.now(),
+    is_canli_bahis = open_bet.bet_ending_datetime <= datetime.datetime.now()
     return flask.render_template("bahis/bahis_detay_yeni.html", open_bet=open_bet, from_frame=from_frame,
                                  is_canli_bahis=is_canli_bahis, datestring=str(open_bet.bet_ending_datetime).replace(" ", "T"))
 
